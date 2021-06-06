f9f4337eed7a702a3df3a1e8fb360769d00ff599
incinerator17 <punarvpawade17@gmail.com>
2021-06-06 20:00:05 +0530

    basic page switch and misc

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function recieveView() {
+    var orderPage = document.getElementById("orderpage")
+    var recievePage = document.getElementById("recievepage")
+    recievePage.style.display = "block"
+    orderPage.style.display = "none"
+}
+function orderView() {
+    var orderPage = document.getElementById("orderpage")
+    var recievePage = document.getElementById("recievepage")
+    orderPage.style.display = "block"
+    recievePage.style.display = "none"
+}
