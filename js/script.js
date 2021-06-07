72a20e8935e719ab20cf0f7f2157437c9d80ed22
incinerator17 <punarvpawade17@gmail.com>
2021-06-07 11:03:52 +0530

    pass input value to reciever page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
+const orderPage = document.getElementById("orderpage")
+const recievePage = document.getElementById("recievepage")
+const inputArea = document.getElementById("addOrder")
+const submitButton = document.getElementById("submit")
+var outputText = document.getElementsByClassName("recieve")
+
+
 function recieveView() {
-    var orderPage = document.getElementById("orderpage")
-    var recievePage = document.getElementById("recievepage")
     recievePage.style.display = "block"
     orderPage.style.display = "none"
 }
 function orderView() {
-    var orderPage = document.getElementById("orderpage")
-    var recievePage = document.getElementById("recievepage")
     orderPage.style.display = "block"
     recievePage.style.display = "none"
+}
+
+function eventListeners() {
+    //Form Submission
+    document.querySelector('#form').addEventListener('submit', newTask);
+
+    //Remove tasks from TaskList
+    taskList.addEventListener('click', completeTask);
+
+    //Remove tasks from TaskList
+    taskList.addEventListener('click', removeTask);
+
+    //Document
+    document.addEventListener('DOMContentLoaded', localStorageOnLoad);
+}
+
+
+submitButton.addEventListener("click", submitClick)
+
+function submitClick(e) {
+    var t = document.getElementById("recievepage")
+    t.innerHTML = (inputArea.value)
+    inputArea.value = ""
 }
